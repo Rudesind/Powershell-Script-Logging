c94c46a9417d7124288826f35e44c195aac6d57c
Zach <32909572+Rudesind@users.noreply.github.com>
2018-11-06 09:43:26 -0800

    Added files
    Added project files.

---
+++ Log/LOG.md
@@ -0,0 +1,56 @@
+# LOG
+
+---
+Module: LOG.psm1
+Updated: 10/12/2018
+Author: Configuration Management
+Version: 1.0
+
+This module is used to create and write to a log file. This log file can used to document any type of process.
+
+Use `Get-Help` to view information for a particular function.
+
+NOTE: When creating a modular function, Powershell prefers to use certain verbs. Use `Get-Verb` to see a list of preferred verbs.
+
+Disclosure:
+The idea for the below function was taken from the following article:
+https://stackoverflow.com/questions/7834656/create-log-file-in-powershell
+
+## Installation
+
+```powershell
+Import-Module <path>\LOG.psm1
+```
+
+## Syntax
+
+```powershell
+Initialize-Log [-File] <path> [-Name] <name>
+```
+
+```powershell
+Write-Log [-LogMessage] <message>
+```
+
+## Examples
+
+```powershell
+Initialize-Log ".\" "Test_Log"
+
+Write-Log "Logging Start"
+```
+
+## Error Codes
+
+(-1): The script could not be initialized.
+Initialize-Log - LOG_INITIALIZE_FAILED (4000): Could not initialize the log file
+Write-Log - LOG_WRITE_FAILED (4000): Could not write to the log file
+
+## References
+
+[Usage Syntax](https://stackoverflow.com/questions/9725675/is-there-a-standard-format-for-command-line-shell-help-text)
+[CmdletBinding](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_cmdletbindingattribute?view=powershell-6)
+[Advanced Function Parameters](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-6)
+[Anatomy of a PowerShell Advanced Function](https://www.petri.com/anatomy-powershell-advanced-function)
+[Script Modules](https://stackoverflow.com/questions/27138483/how-can-i-re-use-import-script-code-in-powershell-scripts)
+[Powershell Scope](https://stackoverflow.com/questions/14439431/variables-in-modules-in-powershell)
---
+++ Log/Legacy/LOG_Legacy.psm1
@@ -0,0 +1,207 @@
+<#
+    Module : LOG_Legacy.psm1
+    Updated: 10/22/2018
+    Author : Configuration Management
+    Version: 1.0
+    Documentation: LOG_Legacy.md
+
+    Summary:
+    This module is used to handle all functions related to logging scripting
+    actions. See the documentation for additional details. This legacy version
+    is for POSReady 2009 and POSReady 7 devices.
+
+    Disclosure: 
+    The idea for the below function was taken from the following article:
+    https://stackoverflow.com/questions/7834656/create-log-file-in-powershell
+#>
+
+Function Initialize-Log {
+    <#
+    .Synopsis
+        This function initializes a log file. Note, the file is not created.
+    .Description
+        This function is part of the LOG module. This module is used to to initialize and write to a specified log file. All log files are created in .log format.
+        
+    .Notes
+        Module : LOG_Legacy.psm1
+        Updated: 10/22/2018
+        Author : Configuration Management
+        Version: 1.0
+        Documentation: LOG_Legacy.md
+
+        Disclosure: 
+        The idea for the below function was taken from the following article:
+        https://stackoverflow.com/questions/7834656/create-log-file-in-powershell
+        
+    .Inputs
+        System.String
+    
+    .Outputs
+        System.Int32
+    
+    .Parameter Path
+        The directory where the log file will be created.
+
+    .Parameter Name
+        The name of the log file to create.
+
+    .Example
+        Initialize-Log "C:\" "TestLog"
+    
+    #>
+
+    # Allows the script to operate like a compiled cmdlet
+    #
+    [CmdletBinding()] 
+
+    # The inner comments of the Param block will be displayed with: Get-Help ... -Detailed if no Parameter section is defined
+    #
+    Param(
+
+        # Cannot be $null or ""
+        # Path to the file must exist 
+        # Parameter is mandatory and allows piped data 
+        #
+        [ValidateNotNullOrEmpty()] 
+        [ValidateScript({(Test-Path $_)})]
+        [Parameter(ValueFromPipeline=$True, Mandatory=$True)]
+        [string] $Path,
+
+        [ValidateNotNullOrEmpty()] 
+        [Parameter(Mandatory=$True)]
+        [string] $Name
+    )
+
+    # Error Codes
+    #
+    New-Variable LOG_INITIALIZE_FAILED -option Constant -value 4000
+
+    # Initialize the function 
+    #
+    try {
+
+        # Time stamp is in ISO 8601 standard
+        #
+        [string] $timeStamp = Get-Date -format "yyyyMMddTHHmmss"
+
+        # Friendly error message for the function
+        #
+        [string] $errorMsg = [string]::Empty
+
+    } catch {
+        throw "Error initializing function"
+        return -1
+    }
+
+
+    # Create and initialize log file
+    #
+    try {
+
+        # Create the script variable for the log file
+        #
+        $script:Log = $Path + "\" + $Name + "_" + $timeStamp + ".log"
+
+    } catch {
+        $errorMsg = "Error initializing file: " + $Error[0]
+        throw 
+        return $LOG_INITIALIZE_FAILED 
+    }
+
+    return 0
+
+}
+
+Function Write-Log {
+    <#
+    .Synopsis
+        This function writes to a defined file with a specific format.
+    .Description
+        This function is part of the LOG module. This module is used to to initialize and write to a specified log file.
+        
+    .Notes
+        Module : LOG_Legacy.psm1
+        Updated: 10/22/2018
+        Author : Configuration Management
+        Version: 1.0
+        Documentation: LOG_Legacy.md
+
+        Disclosure: 
+        The idea for the below function was taken from the following article:
+        https://stackoverflow.com/questions/7834656/create-log-file-in-powershell
+        
+    .Inputs
+        System.String
+    
+    .Outputs
+        System.Int32 
+    
+    .Parameter File
+        The log message to write
+
+    .Example
+        Write-Log "Beginning the process..."
+    
+    #>
+
+    # Allows the script to operate like a compiled cmdlet
+    #
+    [CmdletBinding()] 
+
+    # The inner comments of the Param block will be displayed with: Get-Help ... -Detailed if no Parameter section is defined
+    #
+    Param(
+
+        # Cannot be $null or ""
+        #
+        [ValidateNotNullOrEmpty()] 
+
+        # Parameter is mandatory and allows piped data 
+        #
+        [Parameter(ValueFromPipeline=$True, Mandatory=$True)]
+
+        [string] $LogMessage
+    )
+
+    # Error Codes
+    #
+    New-Variable LOG_WRITE_FAILED -option Constant -value 4000
+
+    # Initialize the function 
+    #
+    try {
+
+
+        # The log time variable
+        #
+        [string] $timeStamp = Get-Date -Format g
+    
+        # Friendly error message for the function
+        #
+        [string] $errorMsg = [string]::Empty
+
+    } catch {
+        throw "Error initializing function"
+        return -1
+    }
+
+
+    # Begin writing to log file
+    #
+
+    try {
+
+        # Write the log message to the log file
+        #
+        Add-Content $script:Log -value "[$timeStamp]`t$LogMessage" -ErrorAction Stop
+
+    } catch {
+        $errorMsg = "Error writing to log: " + $Error[0]
+        throw 
+        return $LOG_WRITE_FAILED
+    }
+
+    return 0
+}
+
+
